6df5a50b81a1c524316e0b1d24cec91d041bd3a2
Guillaume Vincent <guillaume.vincent@carestream.com>
2015-02-05 16:36:21 +0100

    delete file

--- footer.php
+++ footer.php
@@ -31,25 +31,19 @@
 </section>
 
 
-
-<!-- jQuery is called via the WordPress-friendly way via functions.php -->
 <script src="<?php echo get_template_directory_uri(); ?>/js/jquery.min.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/js/bootstrap.min.js"></script>
 
-<!-- Asynchronous google analytics; this is the official snippet.
-         Replace UA-XXXXXX-XX with your site's ID and domainname.com with your domain, then uncomment to enable.
-
 <script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-XXXXXX-XX', 'domainname.com');
-  ga('send', 'pageview');
+	ga('create', 'UA-58598370-1', 'auto');
+	ga('send', 'pageview');
 
 </script>
--->
 
 </body>
 
